d8ea8251b0bdf859bc0ce77a79b2422b55f5b44d
Chase Paules <cpaules@gmail.com>
2017-11-06 20:50:32 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,33 +11,33 @@ class SongsController < ApplicationController
   # def new
   #   @song = Song.new
   # end
-  # 
+  #
   # def create
   #   @song = Song.new(song_params)
-  # 
+  #
   #   if @song.save
   #     redirect_to @song
   #   else
   #     render :new
   #   end
   # end
-  # 
+  #
   # def edit
   #   @song = Song.find(params[:id])
   # end
-  # 
+  #
   # def update
   #   @song = Song.find(params[:id])
-  # 
+  #
   #   @song.update(song_params)
-  # 
+  #
   #   if @song.save
   #     redirect_to @song
   #   else
   #     render :edit
   #   end
   # end
-  # 
+  #
   # def destroy
   #   @song = Song.find(params[:id])
   #   @song.destroy
@@ -51,4 +51,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
