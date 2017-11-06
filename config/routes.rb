ff8b3380e5299e80224f2d2996d8f30f2dea51ac
Chase Paules <cpaules@gmail.com>
2017-11-06 20:55:40 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   resources :artists, only: [:index]
-  resources :songs, only: [:index, :show] 
+  resources :songs, only: [:index, :show]
 
   get '/songs/new', to: 'songs#show'
   get '/songs/edit', to: 'songs#show'
+
+  get '/artists/new', to: 'artists#show'
+  get '/artists/edit', to: 'artists#show'
 
   root to: 'artists#index'
 end
