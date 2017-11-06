c46afc137e034ba5499dbf441c4b9335a1070fc9
Chase Paules <cpaules@gmail.com>
2017-11-06 20:55:24 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  resources :artists, only: [:index] 
+  resources :artists, only: [:index]
   resources :songs, only: [:index, :show] 
-  
-  get '/patients/:id', to: 'patients#show'
+
+  get '/songs/new', to: 'songs#show'
+  get '/songs/edit', to: 'songs#show'
 
   root to: 'artists#index'
 end
